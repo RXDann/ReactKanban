156a7763e995db2ef1e3bd0b62fa53b2b629fa63
Daniel Henrique <danielsilva27@gmail.com>
2020-12-03 13:49:48 -0300

    Tasklist Ajustes e adicionado contents.

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./tasklist.css";
 
-export default function TaskList(props){
-return(
-  <div className="tasklist">
-    <div>{props.title}</div>
-  </div>
-)
-} 
+export default function TaskList({ title }) {
+  return (
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content"></div>
+    </div>
+  );
+}
+
