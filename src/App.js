f20f4c13e4ec69eb745bb56a6df8e9efa5470898
Daniel Henrique <danielsilva27@gmail.com>
2020-12-03 09:53:46 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "./components/Navbar/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <ul className="lista">
+        <li>Testando</li>
+        <li>lista </li>
+      </ul>
+    </div>
+  );
+}
